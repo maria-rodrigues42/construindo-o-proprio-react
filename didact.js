2dd58ef660f7142ee0dc74a9135b4cdb5dadd306
Erick Oliveira <108440487+erickodantas@users.noreply.github.com>
2026-06-09 20:11:47 +0000

    missao2: fiber tree e work loop

--- didact.js
+++ didact.js
@@ -1,42 +1,98 @@
 function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(child =>
-        typeof child === "object"
-          ? child
-          : createTextElement(child)
-      ),
-    },
+    return {
+      type,
+      props: {
+        ...props,
+        children: children.map(child =>
+          typeof child === "object"
+            ? child
+            : createTextElement(child)
+        ),
+      },
+    }
   }
-}
 
-function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
+  function createTextElement(text) {
+    return {
+      type: "TEXT_ELEMENT",
+      props: {
+        nodeValue: text,
+        children: [],
+      },
+    }
   }
-}
 
-function render(element, container) {
-  const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type)
+  function render(element, container) {
+    const dom =
+      element.type === "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(element.type)
 
-  Object.keys(element.props)
-    .filter(key => key !== "children")
-    .forEach(name => {
-      dom[name] = element.props[name]
-    })
+    Object.keys(element.props)
+      .filter(key => key !== "children")
+      .forEach(name => {
+        dom[name] = element.props[name]
+      })
 
-  element.props.children.forEach(child =>
-    render(child, dom)
-  )
+    element.props.children.forEach(child =>
+      render(child, dom)
+    )
 
-  container.appendChild(dom)
-}
+    container.appendChild(dom)
+  }
+
+  function createDom(fiber) {
+    const dom =
+      fiber.type === "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(fiber.type)
+
+    updateDom(dom, {}, fiber.props)
+    return dom
+  }
+
+  let nextUnitOfWork = null
+
+  function workLoop(deadline) {
+    let shouldYield = false
+    while (nextUnitOfWork && !shouldYield) {
+      nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+      shouldYield = deadline.timeRemaining() < 1
+    }
+
+    if (!nextUnitOfWork && wipRoot) {
+      commitRoot()
+    }
+
+    requestIdleCallback(workLoop)
+  }
+
+  requestIdleCallback(workLoop)
+
+  function performUnitOfWork(fiber) {
+    const isFunctionComponent = fiber.type instanceof Function
+    if (isFunctionComponent) {
+      updateFunctionComponent(fiber)
+    } else {
+      updateHostComponent(fiber)
+    }
+
+    if (fiber.child) {
+      return fiber.child
+    }
+
+    let nextFiber = fiber
+    while (nextFiber) {
+      if (nextFiber.sibling) {
+        return nextFiber.sibling
+      }
+      nextFiber = nextFiber.parent
+    }
+  }
+
+  function updateHostComponent(fiber) {
+    if (!fiber.dom) {
+      fiber.dom = createDom(fiber)
+    }
+    reconcileChildren(fiber, fiber.props.children)
+  }
